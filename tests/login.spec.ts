77578c845d4435a73223f5f1be700d1b45982883
Anurag Mingwal <anuragmingwal@Anurags-MacBook-Pro.local>
2024-09-05 17:06:42 +0530

    changeUpdated

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -36,32 +36,3 @@ test('Login Success', async ({ page }) => {
   });
 
 
-  //incorrect username and password 
-// test('Wrong username', async ({ page }) => {
-//     const loginpage = new Loginpage(page);
-//     await loginpage.gotoUrl();
-//     await loginpage.login("standard_","secret_sauce");
-//     expect(await page.locator("[text='Sauce Labs Backpack']")).toBeTruthy();
-
-//   });
-
-// this is for home inventory 
-// test('Home inventory', async ({ page }) => {
-//     const homepage = new Homepage(page);
-
-//     await homepage.productAdd();
-    
-
-//   });
-/// this is for check out
-//   test('Checkout Delivery', async ({ page }) => {
-//     const checkoutpage = new Checkoutpage(page);
-//     await checkoutpage.clickbuttonCheckout();
-//     await checkoutpage.useraddressFill("Anurag", "Mingwal", 110085);
-//     await checkoutpage.finishClick();
-
-
-
-//   });
-
-//});
